a35f14b6eeeea4de1090487287590e31ea13cff7
League Student <league@iMac-9.attlocal.net>
2019-12-06 17:59:48 -0800

    Morning zombie

---
+++ src/_03_morning_zombie/MorningZombie.java
@@ -0,0 +1,17 @@
+package _03_morning_zombie;
+
+import javax.swing.JOptionPane;
+
+public class MorningZombie {
+public static void main(String[] args) {
+	JOptionPane.showMessageDialog(null,"This is my morning routine");
+	JOptionPane.showMessageDialog(null,"1.First I wake up.");
+	JOptionPane.showMessageDialog(null,"2.Take a shower.");
+	JOptionPane.showMessageDialog(null,"3.Change into school clothes.");
+	JOptionPane.showMessageDialog(null,"4.do my hair");
+	JOptionPane.showMessageDialog(null,"5.Eat breakfast.");
+	JOptionPane.showMessageDialog(null,"6.Brush my teeth.");
+	JOptionPane.showMessageDialog(null,"7.Go to school(sadly)");
+	
+}
+}
